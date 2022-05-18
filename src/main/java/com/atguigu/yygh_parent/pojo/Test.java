24bb96bbccdd78fa9daaf0a78ab67464332a94b7
cache <2636105033@qq.com>
2022-05-18 16:33:53 +0800

    hot-fix  test

--- src/main/java/com/atguigu/yygh_parent/pojo/Test.java
+++ src/main/java/com/atguigu/yygh_parent/pojo/Test.java
@@ -3,5 +3,6 @@ package com.atguigu.yygh_parent.pojo;
 public class Test {
     public static void main(String[] args) {
         System.out.println("hello git hot-fix");
+        System.out.println("hello git hot-fix test");
     }
 }
