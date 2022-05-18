e42310d18dbac4b1d0df12dd3ef67aaa9975b488
cache <2636105033@qq.com>
2022-05-18 16:31:02 +0800

    hot-fix first commit

--- src/main/java/com/atguigu/yygh_parent/pojo/Test.java
+++ src/main/java/com/atguigu/yygh_parent/pojo/Test.java
@@ -2,6 +2,6 @@ package com.atguigu.yygh_parent.pojo;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("hello github2222!");
+        System.out.println("hello git hot-fix");
     }
 }
